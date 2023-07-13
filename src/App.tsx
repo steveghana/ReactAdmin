86302cee243a59b226e35a06186c2ff05dc6df6d
steveghana <boatengstephen707@gmail.com>
2023-07-13 13:48:21 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 // import * as React from "react";
-import { render } from "react-dom";
 import { Route } from "react-router-dom";
 import { Admin, CustomRoutes, Resource } from "react-admin";
 import Locations from "./components";
-import DoorsComponent from "./components/Doors";
-
 import dataprovider from "./dataProvider";
 import CommentEdit from "./components/CommentEdit";
 const App = () => (
-  <Admin
-    // title=""
-    dataProvider={dataprovider}
-  >
+  <Admin dataProvider={dataprovider}>
     <CustomRoutes noLayout>
       <Route path="/locations/:id" element={<CommentEdit />} />
     </CustomRoutes>
@@ -19,7 +13,6 @@ const App = () => (
       <Route path="/workers/:id" element={<CommentEdit />} />
     </CustomRoutes>
     <Resource name="locations" list={Locations} />
-    <Resource name="doors" list={DoorsComponent} />
   </Admin>
 );
 
