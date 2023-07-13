9525a1f22bf4918473b658a060053263cc1cf391
steveghana <boatengstephen707@gmail.com>
2023-07-13 14:19:10 +0000

    edit workers pagination and filtering

--- src/components/Workers.tsx
+++ src/components/Workers.tsx
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
 import { List, Datagrid, TextField, EditButton } from "react-admin";
 import { Box, Pagination, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { TextField as Field } from "@mui/material";
 import useSearchFilter from "../customHook";
 
 interface WorkersComponentProps {
   workers: any[];
 }
+interface Location {
+  id: number;
+  location: string;
+  city: string;
+  address: string;
+}
 
 const WorkersComponent: React.FC<WorkersComponentProps> = ({ workers }) => {
-  const [currentPage, setCurrentPage] = React.useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
   const itemsPerPage = 10;
   const handlePageChange = (event: any, newPage: number) => {
     setCurrentPage(newPage);
   };
+
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = workers.slice(indexOfFirstItem, indexOfLastItem);
-  const totalPages = Math.ceil(workers?.length / itemsPerPage);
+  let location = window.origin;
+  const totalPages = Math.ceil(workers.length / itemsPerPage);
   const [data, searchTerm, handleSearch] = useSearchFilter(currentItems);
-
+  console.log(data);
+  // console.log(searchTerm);
   // const formattedTime = timestamp
   //   ? new Date(timestamp).toLocaleTimeString()
   //   : "";
@@ -40,13 +51,21 @@ const WorkersComponent: React.FC<WorkersComponentProps> = ({ workers }) => {
         onChange={(e) => handleSearch(e.target.value)}
       />
       <List pagination={false}>
-        <Datagrid data={data} rowClick="edit">
+        <Datagrid
+          data={data.length < currentItems.length ? data : currentItems}
+          rowClick="edit"
+        >
           <TextField source="name" sortable={true} label="Worker" />
           <TextField source="email" sortable={true} label="Email" />
           <TextField source="phoneNumber" sortable={true} label="Phone" />
+          {/* <TimestampField source="updatedAt" /> */}
           <TextField source="updatedAt" sortable={true} label="Last unlock" />
         </Datagrid>
-        <EditButton />
+        <EditButton
+        // onClick={() => {
+        //   navigate(`${location}/#/workers`);
+        // }}
+        />
         <Box display="flex" justifyContent="center" marginTop={2}>
           <Pagination
             count={totalPages}
