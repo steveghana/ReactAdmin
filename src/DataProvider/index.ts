2d02a0f1e150ade184b66ef86961f6a9b9b32f07
steveghana <boatengstephen707@gmail.com>
2023-07-16 10:43:01 +0000

    add prefix to env

--- src/DataProvider/index.ts
+++ src/DataProvider/index.ts
@@ -1,7 +1,6 @@
-//Git ignore was used to prevent TS from throwing errors as there is no type declaration for react-admin-lb4
 //@ts-ignore
 import rest from 'react-admin-lb4';
-export const Apiurl = import.meta.env.API_URL;
+export const Apiurl = import.meta.env.VITE_URL;
 console.log(Apiurl);
 export const RestProvider = rest(Apiurl);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-            'process.env.YOUR_STRING_VARIABLE': JSON.stringify(env.API_URL),
-        },
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
 });
